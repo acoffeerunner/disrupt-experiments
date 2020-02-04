7b483e63bfc52459fdeeaa6bade6d667302950a8
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-04 23:34:29 +0900

    atmel_mxt_ts: remove unused memory allocation
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -2548,7 +2548,6 @@ static int mxt_check_reg_init(struct mxt_data *data)
 	int ret = 0;
 	const char *config_name = NULL;
 	bool is_recheck = false, use_default_cfg = false;
-	u8 *tp_maker = NULL;
 
 	if (data->firmware_updated)
 		use_default_cfg = true;
@@ -2622,10 +2621,6 @@ start:
 			dev_err(dev, "No lockdown info stored\n");
 		}
 	}
-
-	tp_maker = kzalloc(20, GFP_KERNEL);
-	if (tp_maker == NULL)
-		dev_err(dev, "fail to alloc vendor name memory\n");
 
 	config_name = mxt_get_config(data, use_default_cfg);
 
