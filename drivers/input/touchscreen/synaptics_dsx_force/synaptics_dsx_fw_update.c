61a68178def81cce6a71fd1c43b318dac8fe274d
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-04 23:43:20 +0900

    synaptics_dsx_force: remove unused memory allocation

--- drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_fw_update.c
+++ drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_fw_update.c
@@ -2205,7 +2205,6 @@ static int fwu_read_f34_queries(void)
 	else
 		retval = fwu_read_f34_v5v6_queries();
 
-	update_hardware_info(TYPE_TP_MAKER, rmi4_data->lockdown_info[0] - 0x30);
 	return retval;
 }
 
